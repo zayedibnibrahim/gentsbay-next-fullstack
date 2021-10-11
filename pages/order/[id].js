fa9b3b983e11e59b24e594033191ab8d2b11e7e8
zayedibnibrahim <zayed.ibn@gmail.com>
2021-10-11 23:35:36 +0600

    Added order history

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useContext, useEffect, useReducer } from 'react'
 import dynamic from 'next/dynamic'
 import Layout from '../../components/Layout'
