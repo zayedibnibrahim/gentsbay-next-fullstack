55a1fb08f2e8c2c647b99a29b836b79cefc97420
zayedibnibrahim <zayed.ibn@gmail.com>
2021-10-13 03:12:36 +0600

    Fix issue for deploy to vercel

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -82,6 +82,11 @@ function AdminOrders() {
                   <ListItemText primary='Orders'></ListItemText>
                 </ListItem>
               </NextLink>
+              <NextLink href='/admin/products' passHref>
+                <ListItem button component='a'>
+                  <ListItemText primary='Products'></ListItemText>
+                </ListItem>
+              </NextLink>
               <NextLink href='/admin/users' passHref>
                 <ListItem button component='a'>
                   <ListItemText primary='Users'></ListItemText>
