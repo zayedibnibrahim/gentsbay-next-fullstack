2bae98a2b60831ebdc73d54f6bd0251238290eff
zayedibnibrahim <zayed.ibn@gmail.com>
2021-10-16 03:21:28 +0600

    Fixed shopping cart issue in cookies

--- pages/shipping.js
+++ pages/shipping.js
@@ -13,13 +13,13 @@ import useStyles from '../utils/styles'
 import Cookies from 'js-cookie'
 import { Controller, useForm } from 'react-hook-form'
 import CheckoutWizard from '../components/CheckoutWizard'
-
 export default function Shipping() {
   const {
     handleSubmit,
     control,
     formState: { errors },
     setValue,
+    getValues,
   } = useForm()
   const router = useRouter()
   const { state, dispatch } = useContext(Store)
@@ -27,6 +27,7 @@ export default function Shipping() {
     userInfo,
     cart: { shippingAddress },
   } = state
+  const { location } = shippingAddress
   useEffect(() => {
     if (!userInfo) {
       router.push('/login?redirect=/shipping')
@@ -41,8 +42,9 @@ export default function Shipping() {
   const submitHandler = ({ fullName, address, city, postalCode, country }) => {
     dispatch({
       type: 'SAVE_SHIPPING_ADDRESS',
-      payload: { fullName, address, city, postalCode, country },
+      payload: { fullName, address, city, postalCode, country, location },
     })
+
     Cookies.set(
       'shippingAddress',
       JSON.stringify({
@@ -51,9 +53,31 @@ export default function Shipping() {
         city,
         postalCode,
         country,
+        location,
       })
     )
     router.push('/payment')
+  }
+
+  const chooseLocationHandler = () => {
+    const fullName = getValues('fullName')
+    const address = getValues('address')
+    const city = getValues('city')
+    const postalCode = getValues('postalCode')
+    const country = getValues('country')
+    dispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: { fullName, address, city, postalCode, country },
+    })
+    Cookies.set('shippingAddress', {
+      fullName,
+      address,
+      city,
+      postalCode,
+      country,
+      location,
+    })
+    router.push('/map')
   }
   return (
     <Layout title='Shipping Address'>
@@ -202,6 +226,18 @@ export default function Shipping() {
                 ></TextField>
               )}
             ></Controller>
+          </ListItem>
+          <ListItem>
+            <Button
+              variant='contained'
+              type='button'
+              onClick={chooseLocationHandler}
+            >
+              Choose on map
+            </Button>
+            <Typography>
+              {location.lat && `${location.lat}, ${location.lat}`}
+            </Typography>
           </ListItem>
           <ListItem>
             <Button variant='contained' type='submit' fullWidth color='primary'>
